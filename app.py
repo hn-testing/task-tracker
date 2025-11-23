5efe1ca83d992b03389273ed95c0451ac3c5ac0f
Hemant <hemant@hemant.com>
2025-11-24 00:00:27 +0530

    fix for gunicorn wsgi

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from app import create_app
 
 app = create_app()
 
-# if __name__ == '__main__':
-#     port = int(os.environ.get('PORT', 5000))
-#     debug = os.environ.get('FLASK_DEBUG', '0') in ('1','true','True')
-#     app.run(host='0.0.0.0', port=port, debug=debug)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_DEBUG', '0') in ('1','true','True')
+    app.run(host='0.0.0.0', port=port, debug=debug)
 
-# # to run .\venv\Scripts\Activate; python app.py
+# to run .\venv\Scripts\Activate; python app.py
+
